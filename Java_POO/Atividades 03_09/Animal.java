ee5b0c7cb291ae675483d73dda62f94aa7bfc38f
Guilherme Rodrigues de Morais <grm4023@gmail.com>
2025-09-10 20:03:44 -0300

    Create Animal.java

---
+++ Java_POO/Atividades 03_09/Animal.java
@@ -0,0 +1,9 @@
+class Animal {
+    public String nome;
+    public String peso;
+    public String raca;
+
+    public void emitirSom() {
+        System.out.println("O animal faz um som genérico.");
+    }
+}
